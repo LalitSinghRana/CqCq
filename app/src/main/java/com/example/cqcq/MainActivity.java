10455a34fbef25c5f1e1e3a4963483846f0dceac
LalitSinghRana <lalitrn44@gmail.com>
2021-06-23 14:36:26 +0530

    converted to switch button

--- app/src/main/java/com/example/cqcq/MainActivity.java
+++ app/src/main/java/com/example/cqcq/MainActivity.java
@@ -6,21 +6,47 @@ import androidx.core.content.ContextCompat;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Intent intent;
+    private Switch switchButton;
+//    String min = "5";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        switchButton = (Switch)findViewById(R.id.switch1);
+
+        switchButton.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked == true) {
+                    TextView t = findViewById(R.id.editTextNumber);
+                    String min = t.getText().toString();
+
+                    intent = new Intent(MainActivity.this, MyService.class);
+                    intent.putExtra("minutes", min);
+
+                    Toast.makeText(getApplicationContext(), "Set to " + min, Toast.LENGTH_LONG).show();
+                    t.setText(min);
+
+                    ContextCompat.startForegroundService(MainActivity.this, intent);
+                } else {
+                    Toast.makeText(getApplicationContext(),"Off", Toast.LENGTH_SHORT).show();
+                    stopService(intent);
+                }
+            }
+        });
 
 //        intent = new Intent(this, MyService.class);
     }
 
-    public void startForeSer(View view) {
+    /*public void startForeSer(View view) {
         TextView t = findViewById(R.id.editTextNumber);
 
         String min = t.getText().toString();
@@ -36,5 +62,5 @@ public class MainActivity extends AppCompatActivity {
 
     public void stopForeSer(View view) {
         stopService(intent);
-    }
+    }*/
 }
