e481f1a4b56497dbafc2a7203563b703ab7c4d0e
LalitSinghRana <lalitrn44@gmail.com>
2021-06-28 20:59:56 +0530

    moved back to timer

--- app/src/main/java/com/example/cqcq/MainActivity.java
+++ app/src/main/java/com/example/cqcq/MainActivity.java
@@ -1,28 +1,26 @@
 package com.example.cqcq;
 
-import androidx.annotation.RequiresApi;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Build;
 import android.os.Bundle;
-import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Intent intent;
+    @SuppressLint("UseSwitchCompatOrMaterialCode")
     private Switch switch1;
 
     public static final String SHARED_PREF = "sharedPrefs";
-//    public static final String NEXT_ALARM = "nextAlarm";
     public static final String SWITCH1 = "switch1";
 
     private boolean switchOnOff;
-//    private String getNextAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,28 +33,24 @@ public class MainActivity extends AppCompatActivity {
         loadData();
         switch1.setChecked(switchOnOff);
 
-        switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
-            @RequiresApi(api = Build.VERSION_CODES.O)
-            @Override
-            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                if(isChecked) {
-                    TextView t = findViewById(R.id.editTextNumber);
-                    String min = t.getText().toString();
-                    if(min.matches("")) {
-                        Toast.makeText(getApplicationContext(), "Text field can't be empty", Toast.LENGTH_SHORT).show();
-                        switch1.setChecked(false);
-                    } else {
-                        intent.putExtra("minutes", min);
-                        saveData(true);
-                        Toast.makeText(getApplicationContext(), "Set to repeat after " + min + " minutes", Toast.LENGTH_SHORT).show();
-
-                        ContextCompat.startForegroundService(MainActivity.this, intent);
-                    }
+        switch1.setOnCheckedChangeListener((buttonView, isChecked) -> {
+            if(isChecked) {
+                TextView t = findViewById(R.id.editTextNumber);
+                String min = t.getText().toString();
+                if(min.matches("")) {
+                    Toast.makeText(getApplicationContext(), "Text field can't be empty", Toast.LENGTH_SHORT).show();
+                    switch1.setChecked(false);
                 } else {
-                    saveData(false);
-                    Toast.makeText(getApplicationContext(),"Stopped alarm", Toast.LENGTH_SHORT).show();
-                    stopService(intent);
+                    intent.putExtra("minutes", min);
+                    saveData(true);
+                    Toast.makeText(getApplicationContext(), "Set to repeat after " + min + " minutes", Toast.LENGTH_SHORT).show();
+
+                    ContextCompat.startForegroundService(MainActivity.this, intent);
                 }
+            } else {
+                saveData(false);
+                Toast.makeText(getApplicationContext(),"Stopped alarm", Toast.LENGTH_SHORT).show();
+                stopService(intent);
             }
         });
     }
